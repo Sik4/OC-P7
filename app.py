bd2772661aebb745b568089c75c911d83c5a5831
Sik4 <36238495+Sik4@users.noreply.github.com>
2019-01-25 10:30:32 +0100

    parser ONLINE !
    status : 
    AJAX : OK
    API : OK 
    MAP : OK 
    MARKER : OK 
    PARSER : OK \o/ woohooooo
    TEST : TODO

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
 from flask import Flask, request, render_template, jsonify
 import json
+import wikipedia
+import re
+from functions import __parser__
+
 
 app = Flask(__name__)
 
+# put an ico icon for favicon error
 # with open('fr.json', 'r') as fp:
 #   town_list = json.load(fp)
 
@@ -20,16 +25,22 @@ def town_list_process():
 
     try:
         lang = request.args.get("proglang", type=str)
-        user_input = str(lang).lower()
+        input = str(lang).lower()
+        user_input = __parser__(input)
         for cities in town_list:
             print(cities["city"])
-            if cities["city"].lower() == user_input:
+            handel = __parser__(cities["city"].lower())
+            if re.match(r".*" + handel + ".*", user_input):
                 latitude = cities["lat"]
                 longitude = cities["lng"]
-                API_KEY = "AIzaSyBr59ta9mMfBqCcmIUysOKcGt9uHqgy-Qk"
+                wikipedia.set_lang("fr")
+                ville = cities["city"]  # or lang but wiki is case sensitive for OpenClassrooms
+                pageville = wikipedia.WikipediaPage(ville)
+                API_KEY = "AIzaSyBkKrzwEFfXOQfGR46Qn1KZoUjN7oZ8Sg0"
                 url = "https://maps.googleapis.com/maps/api/staticmap?center=" + user_input + "&zoom=12&size=400x400" \
-                "&markers=color:blue%7C"+ latitude + "%7C" + longitude + "key=" + API_KEY
-                return jsonify(result="this is a french city", mapurl=url)
+                "&maptype=roadmap&markers=color:blue%7C"+ latitude + "," + longitude + "&key=" + API_KEY
+                wikiresult = pageville.summary
+                return jsonify(result=("D'ailleurs, savais tu que ", wikiresult, "?"), mapurl=url)
                 # return jsonify({'output': render_template('index.html', result="this is a french city", mapurl=url)})
 
     except:
